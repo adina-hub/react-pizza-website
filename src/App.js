2e736579cd8d723d031c3c0c3c2c3df61e620166
finy_ber <finy_ber@yahoo.com>
2021-01-20 12:06:35 +0200

    Added Products component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { Hero } from "./components/Hero";
+import { Products } from "./components/Products";
 import { GlobalStyle } from "./globalStyles";
+import { productData } from './components/Products/data';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading='Choose your favorite' data={productData}/>
     </Router>
   );
 }
